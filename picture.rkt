5c4f2e83cd2036ca2b2d5d0fa209436454a19f60
MartinRudzki <martin.rudzki@yahoo.com>
2016-04-17 22:28:19 -0400

    player_object

--- picture.rkt
+++ picture.rkt
@@ -9,15 +9,15 @@
 
 ;Castle with towers
 (define victorian                       ; Left rectangle                        
-  (above (overlay/offset(overlay/offset (above (triangle 100 "solid" "red")(rectangle 60 80 "solid" "dimgray"))190 0
+  (above (overlay/offset(overlay/offset (above (triangle 60 "solid" "red")(rectangle 30 40 "solid" "dimgray"))190 0
                                         ; middle Rectangle
-                                        (above (triangle 100 "solid" "red")(rectangle 60 80 "solid" "dimgray")))280 0
+                                        (above (triangle 60 "solid" "red")(rectangle 30 40 "solid" "dimgray")))280 0
                                         ;right rectangle
-                                        (above (triangle 100 "solid" "red")(rectangle 60 80 "solid" "dimgray")))
-         (rectangle 600 150 "solid" "dimgray")))
+                                        (above (triangle 60 "solid" "red")(rectangle 30 40 "solid" "dimgray")))
+         (rectangle 600 100 "solid" "dimgray")))
 ;so we can call door
-(define door (overlay/align "center" "bottom" (rectangle 60 60 "solid" "brown")
-                            (rotate 180(ellipse 60 100 "solid" "brown"))))
+(define door (overlay/align "center" "bottom" (rectangle 40 40 "solid" "brown")
+                            (rotate 180(ellipse 40 60 "solid" "brown"))))
 ;TO DO: Use (frame...) for bars
 (define door-with-knob
   (overlay/align "center" "center" (circle 6 "solid" "yellow")  door))
@@ -35,30 +35,30 @@
                       (beside
                        (beside
                         (beside
-                         (beside(rectangle 70 60 "solid" "blue")
+                         (beside(rectangle 70 45 "solid" "blue")
          
-                               (rectangle 90 60 "solid" (make-color 75 54 33)))
-                         (rectangle 95 60 "solid" "blue"))
-                        (rectangle 90 60 "solid" (make-color 75 54 33)))
-                       (rectangle 95 60 "solid" "blue"))
-                      (rectangle 90 60 "solid" (make-color 75 54 33)))
-                     (rectangle 70 60 "solid" "blue")))
+                               (rectangle 90 45 "solid" (make-color 75 54 33)))
+                         (rectangle 95 45 "solid" "blue"))
+                        (rectangle 90 45 "solid" (make-color 75 54 33)))
+                       (rectangle 95 45 "solid" "blue"))
+                      (rectangle 90 45 "solid" (make-color 75 54 33)))
+                     (rectangle 70 45 "solid" "blue")))
 
-(define middle_board(above(above water_bridge (rectangle 600 600 "solid" "darkgreen"))water_bridge))
+(define middle_board(above(above water_bridge (rectangle 600 300 "solid" "darkgreen"))water_bridge))
 
 ;
 (define game_board(overlay/align "center" "top" (above
                    (above
-                    ;Left Castle
+                    ;Top Castle
                     castle
                     ;Middle Game board piece
                     middle_board)
-                   ;Right caslte Piece
+                   ;Bottomt caslte Piece
                     (rotate 180 castle))
                      ;Game board bottom      
-                    (overlay/align "center" "bottom" (overlay/offset (place-image (text "Player 1" 24 "white") 75 25 (rectangle 400 180 "solid" "blue"))1000 0  (place-image (text "Player 2" 24 "white") 75 25 (rectangle 400 180 "solid" "red")))(rectangle 1500 1500 "solid" "gray"))))
+                    (overlay/align "center" "bottom" (overlay/offset (place-image (text "Player 1" 24 "white") 75 25 (rectangle 400 180 "solid" "blue"))1050 0  (place-image (text "Player 2" 24 "white") 75 25 (rectangle 400 180 "solid" "red")))(rectangle 1500 825 "solid" "gray"))))
 ;Calling gameboard
-;game_board
+game_board
 
 ;(define player_obj m
 
@@ -74,21 +74,20 @@
 ;var t_count = plater territory count
 ;var active_player  = Turn ON/OFF, can player spend income and build units?
 ;
-;
-;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define (player_object balance)
-  ;Setting default value for territory count
-  (define t_count 0)       ; territory count
-  (define active_player "on") ; "on" active,
-                              ; "off" not active
-
+  ;Setting default variable values 
+  (define t_count 0)           ; territory count
+  (define active_player "on") 
+  ;;;;;;;;;;;;;;;;;;;;;;
+  ;Toggle active_player
+  ; "on" active,
+  ; "off" not active
+  ;;;;;;;;;;;;;;;;;;;;;;
   (define (active_player_toggle)
     (if (eq? "off" active_player)
         (begin (set! active_player "on")active_player)
     (begin (set! active_player "off")active_player)))
-  
-  ;(define (active_player_off) (set! active_player 0)active_player)
   ;;;;;;;;;;;;;;;;
   ;Decrease Income
   ;;;;;;;;;;;;;;;;
@@ -123,11 +122,11 @@
         ;If "on" player can access their menu
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         (cond ((eq? m 'active_player_toggle) active_player_toggle)
-              ((eq? m 'income_dec) income_dec)
-              ((eq? m 'income_inc) income_inc)
-              ((eq? m 'territory_count) territory_count)
-              ((eq? m 'get_territory_count) get_territory_count)
-              ((eq? m 'get_income_count) get_income_count)
+              ((eq? m 'income_dec) income_dec)                    ;Sets income decrease
+              ((eq? m 'income_inc) income_inc)                    ;Sets income increae
+              ((eq? m 'territory_count) territory_count)          ;Sets territory count
+              ((eq? m 'get_territory_count) get_territory_count)  ;return current territory count
+              ((eq? m 'get_income_count) get_income_count)        ;return value of income
               (else
                (error "Unknown Player Request"
                       m)))
@@ -136,11 +135,15 @@
         ;If not then Player is not active.
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         (cond ((eq? m 'active_player_toggle) active_player_toggle)
-              (else (lambda (x) "Player not active")))))
+              (else (lambda (x) "Player not active." )))))
   dispatch)
-
-(define a (player_object 0))
-((a 'territory_count)5)
-((a 'income_inc)10)
-((a 'income_dec)2)
-((a 'territory_count)3)
+;;;;Test code for player object;;;;;;
+;((a 'territory_count)5)
+;((a 'income_inc)10)
+;((a 'income_dec)2)
+;((a 'territory_count)3)
+;((a 'active_player_toggle))
+;((a 'territory_count)7)
+;((a 'active_player_toggle))
+;((a 'get_territory_count))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
